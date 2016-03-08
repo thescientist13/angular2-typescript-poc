4d746ada3f26451519d2e7ce593e75d98c14e29b
Owen Buckley <owen@analogstudios.net>
2016-03-08 01:14:31 -0500

    fix typings install

--- bin/build.sh
+++ bin/build.sh
@@ -3,7 +3,7 @@
 echo "installing dependencies..."
 rm -rf node_modules > /dev/null 2>&1
 npm install
-npm run typings install
+node_modules/typings/dist/bin/typings.js install
 
 echo "running build..."
 gulp build:prod
