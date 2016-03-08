358420cdb270467478d4f153717cd7677078c49e
Owen Buckley <owen@analogstudios.net>
2016-03-07 19:35:52 -0500

    completion of basic Tour of Heroes

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,21 +1,29 @@
-import {Component} from 'angular2/core';
-import {Hero} from './hero';
+import { Component, OnInit } from 'angular2/core';
+import {RouteParams} from 'angular2/router';
+
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
-  inputs: ['hero'],
   selector: 'my-hero-detail',
-  template: `
-    <div *ngIf="hero">
-      <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name"/>
-      </div>
-    </div>
-  `
+  templateUrl: 'app/hero-detail.component.html',
+  styleUrls: ['app/hero-detail.component.css'],
+  inputs: ['hero']
 })
 
-export class HeroDetailComponent {
-  hero: Hero
+export class HeroDetailComponent implements OnInit {
+  hero: Hero;
+
+  constructor(private heroService: HeroService, private routeParams: RouteParams) {
+  }
+
+  ngOnInit() {
+    let id = +this.routeParams.get('id');
+    this.heroService.getHero(id)
+      .then(hero => this.hero = hero);
+  }
+
+  goBack() {
+    window.history.back();
+  }
 }
